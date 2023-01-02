6449ea9b208bcdc67b1af1ddbfb8da218c806314
mo-mohamed <mostafa.moniem90@gmail.com>
2023-01-02 16:01:14 +0200

    IP

--- lib/elixirdock_web/controllers/page_controller.ex
+++ lib/elixirdock_web/controllers/page_controller.ex
@@ -1,7 +1,21 @@
 defmodule ElixirdockWeb.PageController do
   use ElixirdockWeb, :controller
+  require Logger
 
   def index(conn, _params) do
+    forwarded_for = List.first(Plug.Conn.get_req_header(conn, "x-forwarded-for"))
+
+    ip_address =
+      if forwarded_for do
+        String.split(forwarded_for, ",")
+        |> Enum.map(&String.trim/1)
+        |> List.first()
+      else
+        to_string(:inet_parse.ntoa(conn.remote_ip))
+      end
+
+    Logger.info("IP address is: #{ip_address}")
+
     render(conn, "index.html")
   end
 end
